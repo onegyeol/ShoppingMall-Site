e57502e767a535a9b44e3a4f023e2867f8faba92
onegyeol <chlgksruf3414@gmail.com>
2024-07-09 21:33:58 +0900

    초기 빌드

---
+++ src/main/java/project/wideWebsite/WideWebsiteApplication.java
@@ -0,0 +1,13 @@
+package project.wideWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WideWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WideWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/wideWebsite/WideWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package project.wideWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WideWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
