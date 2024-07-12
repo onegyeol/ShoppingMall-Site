94aef5bf397a21c533f93f6d7ab1f7860a30130d
onegyeol <chlgksruf3414@gmail.com>
2024-07-12 16:26:43 +0900

    Item Test (test db는 h2로 사용)

--- src/test/java/project/wideWebsite/domain/ItemRepositoryTest.java
+++ src/test/java/project/wideWebsite/domain/ItemRepositoryTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ItemRepositoryTest {
 
     @Autowired
@@ -17,6 +19,7 @@ public class ItemRepositoryTest {
         Item item = new Item();
         item.setName("test");
         item.setPrice(10000);
+        item.setItemSellStatus(ItemSellStatus.SELL);
 
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
