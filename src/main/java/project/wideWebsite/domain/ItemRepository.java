0b59e42458aeb07b088c36d198e0a80a2fddd17c
onegyeol <chlgksruf3414@gmail.com>
2024-07-17 12:52:54 +0900

    item 클래스 속성 추가

--- src/main/java/project/wideWebsite/domain/ItemRepository.java
+++ src/main/java/project/wideWebsite/domain/ItemRepository.java
@@ -1,6 +1,15 @@
 package project.wideWebsite.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByName(String testProduct1);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
--- src/test/java/project/wideWebsite/domain/ItemRepositoryTest.java
+++ src/test/java/project/wideWebsite/domain/ItemRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class ItemRepositoryTest {
@@ -15,14 +18,32 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("save item test")
-    public void createItemTest(){
-        Item item = new Item();
-        item.setName("test");
-        item.setPrice(10000);
-        item.setItemSellStatus(ItemSellStatus.SELL);
+    public void createItemList(){
+        for(int i=1; i<=10; i++){
+            Item item = new Item();
+            item.setName("test product" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("test product detail" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
 
-        Item savedItem = itemRepository.save(item);
-        System.out.println(savedItem.toString());
+            Item savedItem = itemRepository.save(item);
+            System.out.println(savedItem.toString());
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("find product name")
+    public void findByItemNmTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByName("test product1");
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 }
 
