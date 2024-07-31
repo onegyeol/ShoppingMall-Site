88efd05f03775b5509234e0ac3277158d3fad92e
onegyeol <chlgksruf3414@gmail.com>
2024-07-31 21:33:46 +0900

    @RequiredArgsConstructor 추가

--- src/main/java/project/wideWebsite/controller/ThymeleafExController.java
+++ src/main/java/project/wideWebsite/controller/ThymeleafExController.java
@@ -1,6 +1,7 @@
 package project.wideWebsite.controller;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/main")
+@RequiredArgsConstructor
 public class ThymeleafExController {
 
     @GetMapping
