94408d68b6090fadcd995c4dd379022ca47fb98f
onegyeol <chlgksruf3414@gmail.com>
2024-08-01 15:52:21 +0900

    Item 정보

--- src/main/java/project/wideWebsite/dto/ItemDto.java
+++ src/main/java/project/wideWebsite/dto/ItemDto.java
@@ -7,12 +7,11 @@ import java.time.LocalDateTime;
 
 @Getter @Setter
 public class ItemDto {
-    private Long id;
-    private String itemNm;
-    private Integer price;
-    private String itemDetail;
-    private String sellStatCd;
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
-
+    private Long id; // 아이템의 고유 식별자
+    private String itemNm; // 아이템의 이름
+    private Integer price; // 아이템의 가격
+    private String itemDetail; // 아이템의 상세 설명
+    private String sellStatCd; // 판매 상태 코드 (예: 판매 중, 품절 등)
+    private LocalDateTime regTime; // 아이템 등록 시간
+    private LocalDateTime updateTime; // 아이템 수정 시간
 }
