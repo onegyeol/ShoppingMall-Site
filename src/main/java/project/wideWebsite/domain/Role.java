932ced0c166e44daf55ec892292e6b0d6a9e45da
onegyeol <chlgksruf3414@gmail.com>
2024-08-02 14:24:16 +0900

    회원 권한

--- src/main/java/project/wideWebsite/domain/Role.java
+++ src/main/java/project/wideWebsite/domain/Role.java
@@ -4,11 +4,5 @@ import lombok.Getter;
 
 @Getter
 public enum Role {
-    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
-
-    Role(String value){
-        this.value = value;
-    }
-
-    private String value;
+    USER, ADMIN
 }
