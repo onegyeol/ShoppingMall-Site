70e11578c6d696020c8645afc763a844f38d534a
onegyeol <chlgksruf3414@gmail.com>
2024-08-02 14:25:52 +0900

    사용자 인증 위한 UserDetails 객체 제공

--- src/main/java/project/wideWebsite/config/UserSecurityService.java
+++ src/main/java/project/wideWebsite/config/UserSecurityService.java
@@ -12,8 +12,8 @@ import project.wideWebsite.repository.MemberRepository;
 import project.wideWebsite.domain.Role;
 import project.wideWebsite.service.UserDetailsService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -22,34 +22,25 @@ public class UserSecurityService implements UserDetailsService {
 
     private final MemberRepository memberRepository; // MemberRepository 주입
 
-    // 이메일로 사용자 정보를 조회하고 UserDetails를 반환하는 메서드
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // 이메일로 사용자를 조회합니다.
         Optional<Member> memberOptional = this.memberRepository.findByEmail(email);
 
         // 사용자를 찾지 못한 경우 예외를 던집니다.
-        if(memberOptional.isEmpty()){
+        if (memberOptional.isEmpty()) {
             throw new UsernameNotFoundException("User not found with email: " + email);
         }
 
         // 조회된 사용자 정보를 가져옵니다.
         Member member = memberOptional.get();
 
-        // 권한 리스트를 생성합니다.
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
         // 사용자의 역할에 따라 권한을 설정합니다.
-        if("admin".equals(member.getRole())){
-            // 역할이 "admin"인 경우 ADMIN 권한을 추가합니다.
-            authorities.add(new SimpleGrantedAuthority(Role.ADMIN.getValue()));
-        } else {
-            // 그렇지 않은 경우 USER 권한을 추가합니다.
-            authorities.add(new SimpleGrantedAuthority(Role.USER.getValue()));
-        }
+        Collection<GrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_" + member.getRole().name())
+        );
 
         // UserDetails 객체를 생성하여 반환합니다.
-        // 사용자의 이름, 비밀번호, 권한 리스트를 설정합니다.
-        return new User(member.getName(), member.getPassword(), authorities);
+        return new User(member.getEmail(), member.getPassword(), authorities);
     }
 }
