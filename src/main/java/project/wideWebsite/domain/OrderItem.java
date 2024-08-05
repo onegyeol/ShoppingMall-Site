309ac2390eec357147309ef61f0d49a24a924444
onegyeol <chlgksruf3414@gmail.com>
2024-08-05 16:17:32 +0900

    주문한 아이템 정보

--- src/main/java/project/wideWebsite/domain/OrderItem.java
+++ src/main/java/project/wideWebsite/domain/OrderItem.java
@@ -12,6 +12,14 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id; //주문 항목의 고유 식별자
 
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+    
     private int count; //주문 상품 수량
 
     private int price; //주문 상품의 가격
