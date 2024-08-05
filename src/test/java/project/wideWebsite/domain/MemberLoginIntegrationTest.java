389f1a92db1f5d94b91779c43ab3c443793c35ea
onegyeol <chlgksruf3414@gmail.com>
2024-08-05 16:18:30 +0900

    로그인 테스트

--- src/test/java/project/wideWebsite/domain/MemberLoginIntegrationTest.java
+++ src/test/java/project/wideWebsite/domain/MemberLoginIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -64,4 +65,5 @@ public class MemberLoginIntegrationTest {
                 .andExpect(status().is3xxRedirection())                // 리디렉션 발생
                 .andExpect(redirectedUrl("/members/login/error"));     // 실패 시 리디렉션 URL 확인
     }
+
 }
