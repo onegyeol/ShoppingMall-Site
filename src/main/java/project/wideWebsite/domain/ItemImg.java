5d4203ca1392828e90b8e395803a3a166a0515f7
onegyeol <chlgksruf3414@gmail.com>
2024-08-14 19:29:52 +0900

    상품 이미지 등록

---
+++ src/main/java/project/wideWebsite/domain/ItemImg.java
@@ -0,0 +1,31 @@
+package project.wideWebsite.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.boot.jaxb.mapping.marshall.InheritanceTypeMarshalling;
+
+@Entity
+@Table(name = "ITEM_IMG")
+@Getter @Setter
+public class ItemImg {
+    @Id
+    @Column(name = "ITEM_IMG_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName; //이미지 파일명
+    private String oriImgName; //원본 이미지 파일명
+    private String imgUrl; //이미지 조회 경로
+    private String repimgYn; //대표 이미지 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+}
