66d3503b33db3b734b8bcbd6211fbbac9a7936d1
onegyeol <chlgksruf3414@gmail.com>
2024-08-16 16:06:32 +0900

    상품 저장 서비스

---
+++ src/main/java/project/wideWebsite/service/ItemService.java
@@ -0,0 +1,41 @@
+package project.wideWebsite.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import project.wideWebsite.domain.Item;
+import project.wideWebsite.domain.ItemImg;
+import project.wideWebsite.dto.ItemFormDto;
+import project.wideWebsite.repository.ItemRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        // 상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        // 이미지 등록
+        for(int i=0; i<itemImgFileList.size(); i++){
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if(i==0){
+                itemImg.setRepimgYn("Y");
+            } else {
+                itemImg.setRepimgYn("N");
+            }
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
+}
