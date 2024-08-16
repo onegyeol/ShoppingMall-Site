fbe3941b1c29db97c86699e2c78e5b4759fb9565
onegyeol <chlgksruf3414@gmail.com>
2024-08-16 16:02:56 +0900

    상품 이미지 업로드/저장

---
+++ src/main/java/project/wideWebsite/service/ItemImgService.java
@@ -0,0 +1,40 @@
+package project.wideWebsite.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import project.wideWebsite.domain.ItemImg;
+import project.wideWebsite.repository.ItemImgRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemImgService {
+
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception{
+
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        // 파일 업로드
+        if(!StringUtils.isEmpty(oriImgName)){
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item/" + imgName;
+        }
+
+        // 이미지 저장
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+
+}
