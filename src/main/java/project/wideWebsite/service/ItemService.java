e7858837d15a0551dcb4a3f80cbd6f6279e95b3c
onegyeol <chlgksruf3414@gmail.com>
2024-08-17 18:55:21 +0900

    상품 저장/업데이트 관련 service

--- src/main/java/project/wideWebsite/service/ItemService.java
+++ src/main/java/project/wideWebsite/service/ItemService.java
@@ -1,14 +1,18 @@
 package project.wideWebsite.service;
 
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import project.wideWebsite.domain.Item;
 import project.wideWebsite.domain.ItemImg;
 import project.wideWebsite.dto.ItemFormDto;
+import project.wideWebsite.dto.ItemImgDto;
+import project.wideWebsite.repository.ItemImgRepository;
 import project.wideWebsite.repository.ItemRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +22,7 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
     private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
 
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
 
@@ -36,6 +41,48 @@ public class ItemService {
             }
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
+        return item.getId();
+    }
+
+    @Transactional(readOnly = true) // 단순 조회기능이기 때문에 읽기 전용으로 설정
+    public ItemFormDto getItemDtl(Long itemId){
+
+        // 상품 id를 기반으로 상품 이미지 엔티티 객체 가져옴
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+
+        // 상품 이미지 DTO 객체 담을 그릇 생성
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+        // 상품 이미지 엔티티 객체를 상품 이미지 DTO 객체로 변환(ItemImg -> ItemImgDto)
+        for(ItemImg itemImg : itemImgList){
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        // 상품 id를 기반으로 상품 엔티티 객체 가져옴
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        // 상품 엔티티 객체를 상품 DTO 객체로 변환 (Item -> ItemFormDto)
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        // 상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId()) // 넘어온 ItemFormDto 객체 id로 item 찾음 -> 수정
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+
+        // 상품 이미지 수정
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();    // 넘어온 ItemFormDto 객체 이미지 id로 이미지 찾음 -> 수정
+        for(int i=0; i<itemImgFileList.size(); i++){
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i)); // 수정된 이미지 수만큼 다시 업로드
+        }
+
         return item.getId();
     }
 }
