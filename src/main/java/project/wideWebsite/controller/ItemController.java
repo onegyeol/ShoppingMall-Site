d565c6a10c4b11fa620f8ab53d095162ce883004
hangyeol <chlgksruf3414@gmail.com>
2024-08-20 16:06:39 +0900

    상품 등록/수정 controller

--- src/main/java/project/wideWebsite/controller/ItemController.java
+++ src/main/java/project/wideWebsite/controller/ItemController.java
@@ -3,6 +3,9 @@ package project.wideWebsite.controller;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,10 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import project.wideWebsite.domain.Item;
 import project.wideWebsite.dto.ItemFormDto;
+import project.wideWebsite.dto.ItemSearchDto;
 import project.wideWebsite.service.ItemService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -88,5 +94,16 @@ public class ItemController {
         }
 
         return "form/main";
+    }
+
+    @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page")Optional<Integer> page,Model model){
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
+        return "item/itemMng";
     }
 }
