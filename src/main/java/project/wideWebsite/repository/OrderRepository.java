52108c104f8c0ec709c4f234e52f8cc143d7fbdd
onegyeol <chlgksruf3414@gmail.com>
2024-08-26 16:34:34 +0900

    구매 내역 조회

--- src/main/java/project/wideWebsite/repository/OrderRepository.java
+++ src/main/java/project/wideWebsite/repository/OrderRepository.java
@@ -1,7 +1,23 @@
 package project.wideWebsite.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.wideWebsite.domain.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    // 해당 회원의 구매 이력을 페이징 정보에 맞게 조회
+    @Query("select o from Order o " +
+            "where o.member.email = :email " +
+            "order by o.orderDate desc")
+    List<Order> findOrders(@Param("email") String email, Pageable pageable);
+
+    // 해당 회원의 주문 개수
+    @Query("select count(o) from Order o " +
+            "where o.member.email = :email")
+    Long countOrder(@Param("email") String email);
 }
