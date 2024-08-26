b10ee137bf8061c470ab28b4d6f0927e9adf3a40
onegyeol <chlgksruf3414@gmail.com>
2024-08-26 16:35:07 +0900

    상품 이미지 Repository

--- src/main/java/project/wideWebsite/repository/ItemImgRepository.java
+++ src/main/java/project/wideWebsite/repository/ItemImgRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId); // 이미지가 잘 저장되었는지 확인 위해
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn); // 구매 내역 페이지에서 주문 상품의 대표 이미지를 위한 조회
 }
