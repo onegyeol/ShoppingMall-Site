cc83e594638469e9c9a6b6c61807f8e09e05e9f2
onegyeol <chlgksruf3414@gmail.com>
2024-08-28 20:33:44 +0900

    장바구니 test

---
+++ src/test/java/project/wideWebsite/domain/CartServiceTest.java
@@ -0,0 +1,68 @@
+package project.wideWebsite.domain;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import project.wideWebsite.dto.CartItemDto;
+import project.wideWebsite.repository.CartItemRepository;
+import project.wideWebsite.repository.ItemRepository;
+import project.wideWebsite.repository.MemberRepository;
+import project.wideWebsite.service.CartService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class CartServiceTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    public Item saveItem(){
+        Item item = new Item();
+        item.setItemNm("test product");
+        item.setPrice(10000);
+        item.setItemDetail("test product detail");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember(){
+        Member member = new Member();
+        member.setEmail("test@test.com");
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    public void addCart(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setCount(5);
+        cartItemDto.setItemId(item.getId());
+
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+
+        assertEquals(item.getId(), cartItem.getItem().getId());
+        assertEquals(cartItemDto.getCount(), cartItem.getCount());
+    }
+}
