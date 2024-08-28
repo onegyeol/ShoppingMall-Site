c6c20f79d164f2368d9bcc0daa186fdb344ae7d9
onegyeol <chlgksruf3414@gmail.com>
2024-08-28 20:34:09 +0900

    장바구니 repository

--- src/main/java/project/wideWebsite/repository/CartRepository.java
+++ src/main/java/project/wideWebsite/repository/CartRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.wideWebsite.domain.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByMemberId(Long memberId);
 }
