718308c1c5c303451c2b5e044f860987dac6f7c8
onegyeol <chlgksruf3414@gmail.com>
2024-08-29 17:13:47 +0900

    장바구니 controller

--- src/main/java/project/wideWebsite/controller/CartController.java
+++ src/main/java/project/wideWebsite/controller/CartController.java
@@ -69,4 +69,16 @@ public class CartController {
         cartService.updateCartItemCount(cartItemId, count);
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
+
+    @DeleteMapping("/cartItem/{cartItemId}")
+    @ResponseBody
+    public ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal){
+        if(!cartService.validateCartItem(cartItemId, principal.getName())) {
+            return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+
+        cartService.deleteCartItem(cartItemId);
+
+        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
 }
